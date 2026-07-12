be056d6654cee24844b1a9ccd81140a8094684a7
superson901 <gasparazariel@gmail.com>
2026-07-12 01:02:05 -0400

    background color picker

---
+++ script.js
@@ -0,0 +1,39 @@
+let colors = {
+    1: "blue",
+    2: "red",
+    3: "pink",
+    4: "green",
+    5: "cyan",
+    6: "black",
+    7: "grey",
+    8: "yellow",
+    9: "magenta",
+    10: "darkblue",
+    11: "darkred",
+    12: "darkgreen",
+    13: "darkgrey",
+    14: "darkmagenta",
+    15: "darkorange",
+    16: "darkcyan",
+    17: "darkkhaki",
+    18: "khaki",
+    19: "antiquewhites",
+    20: "white"
+};
+
+let numberPicker = document.getElementById("Spin");
+let currentColor = "";
+numberPicker.onchange = function(){
+
+    let chosenColor = colors[this.value];
+
+        if(currentColor != ""){
+        document.body.classList.remove(currentColor);
+    }
+
+    document.body.classList.add(chosenColor);
+
+    currentColor = chosenColor;
+
+
+}
